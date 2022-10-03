2706ad7ef695d1cd86c4eee84966318404c0c7b8
Shachar Ratzabi <shachar@connecteam.com>
2022-10-03 21:28:35 +0300

    map styling

--- src/components/map-container/map-container.jsx
+++ src/components/map-container/map-container.jsx
@@ -114,6 +114,192 @@ const MapContainer = () => {
         });
     };
 
+    const noDefaultMarkers = {
+        featureType: "poi",
+        elementType: "labels",
+        stylers: [
+            { visibility: "off" }
+        ]
+    }
+
+    const noRoadLabels = {
+        featureType: "road",
+        elementType: "labels",
+        stylers: [
+            { visibility: "off" }
+        ]
+    }
+
+    const darkModeStyles = [
+            { elementType: "geometry", stylers: [{ color: "#242f3e" }] },
+            { elementType: "labels.text.stroke", stylers: [{ color: "#242f3e" }] },
+            { elementType: "labels.text.fill", stylers: [{ color: "#746855" }] },
+            {
+                featureType: "administrative.locality",
+                elementType: "labels.text.fill",
+                stylers: [{ color: "#d59563" }],
+            },
+            {
+                featureType: "poi",
+                elementType: "labels.text.fill",
+                stylers: [{ color: "#d59563" }],
+            },
+            {
+                featureType: "poi.park",
+                elementType: "geometry",
+                stylers: [{ color: "#263c3f" }],
+            },
+            {
+                featureType: "poi.park",
+                elementType: "labels.text.fill",
+                stylers: [{ color: "#6b9a76" }],
+            },
+            {
+                featureType: "road",
+                elementType: "geometry",
+                stylers: [{ color: "#38414e" }],
+            },
+            {
+                featureType: "road",
+                elementType: "geometry.stroke",
+                stylers: [{ color: "#212a37" }],
+            },
+            {
+                featureType: "road",
+                elementType: "labels.text.fill",
+                stylers: [{ color: "#9ca5b3" }],
+            },
+            {
+                featureType: "road.highway",
+                elementType: "geometry",
+                stylers: [{ color: "#746855" }],
+            },
+            {
+                featureType: "road.highway",
+                elementType: "geometry.stroke",
+                stylers: [{ color: "#1f2835" }],
+            },
+            {
+                featureType: "road.highway",
+                elementType: "labels.text.fill",
+                stylers: [{ color: "#f3d19c" }],
+            },
+            {
+                featureType: "transit",
+                elementType: "geometry",
+                stylers: [{ color: "#2f3948" }],
+            },
+            {
+                featureType: "transit.station",
+                elementType: "labels.text.fill",
+                stylers: [{ color: "#d59563" }],
+            },
+            {
+                featureType: "water",
+                elementType: "geometry",
+                stylers: [{ color: "#17263c" }],
+            },
+            {
+                featureType: "water",
+                elementType: "labels.text.fill",
+                stylers: [{ color: "#515c6d" }],
+            },
+            {
+                featureType: "water",
+                elementType: "labels.text.stroke",
+                stylers: [{ color: "#17263c" }],
+            }
+        ]
+
+    const lightModeStyles = [
+        { elementType: "geometry", stylers: [{ color: "#f0eded" }] },
+        { elementType: "labels.text.stroke", stylers: [{ color: "#ffffff" }] },
+        { elementType: "labels.text.fill", stylers: [{ color: "#b9b3b3" }] },
+        {
+            featureType: "administrative.locality",
+            elementType: "labels.text.fill",
+            stylers: [{ color: "#b9b3b3" }],
+        },
+        // {
+        //     featureType: "poi",
+        //     elementType: "labels.text.fill",
+        //     stylers: [{ color: "#ff9800" }],
+        // },
+        // {
+        //     featureType: "poi.park",
+        //     elementType: "geometry",
+        //     stylers: [{ color: "#f2e1e0" }],
+        // },
+        // {
+        //     featureType: "poi.park",
+        //     elementType: "labels.text.fill",
+        //     stylers: [{ color: "#5eff00" }],
+        // },
+        {
+            featureType: "road",
+            elementType: "geometry",
+            stylers: [{ color: "#ffffff" }],
+        },
+        {
+            featureType: "road",
+            elementType: "geometry.stroke",
+            stylers: [{ color: "#ffffff" }],
+        },
+        {
+            featureType: "road",
+            elementType: "labels.text.fill",
+            stylers: [{ color: "#d59563" }],
+        },
+        {
+            featureType: "road.highway",
+            elementType: "geometry",
+            stylers: [{ color: "#ffffff" }],
+        },
+        {
+            featureType: "road.highway",
+            elementType: "geometry.stroke",
+            stylers: [{ color: "#ffffff" }],
+        },
+        {
+            featureType: "road.highway",
+            elementType: "labels.text.fill",
+            stylers: [{ color: "#d59563" }],
+        },
+        {
+            featureType: "transit",
+            elementType: "geometry",
+            stylers: [{ color: "#f2e1e0" }],
+        },
+        {
+            featureType: "transit.station",
+            elementType: "labels.text.fill",
+            stylers: [{ color: "#d59563" }],
+        },
+        {
+            featureType: "water",
+            elementType: "geometry",
+            stylers: [{ color: "#e1dfd3" }],
+        },
+        {
+            featureType: "water",
+            elementType: "labels.text.fill",
+            stylers: [{ color: "#5f5757" }],
+        },
+        {
+            featureType: "water",
+            elementType: "labels.text.stroke",
+            stylers: [{ color: "#ffffff" }],
+        }
+    ]
+
+    const customOptions = {
+        styles:[
+            ...lightModeStyles,
+            noDefaultMarkers,
+            noRoadLabels
+        ]
+    }
+
     return (
         <div className="app" style={{height: "100vh", width: "100%"}}>
             <GoogleMapReact
@@ -122,6 +308,8 @@ const MapContainer = () => {
                 defaultZoom={11}
                 yesIWantToUseGoogleMapApiInternals
                 onGoogleApiLoaded={({map, maps}) => setGoogleMapRef(map, maps)}
+                clickableIcons={false}
+                options={customOptions}
             >
                 {/*{coordinates.map((place, index) => (*/}
                 {/*    <Marker*/}
