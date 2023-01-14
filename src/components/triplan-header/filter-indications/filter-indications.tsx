b5988fe264625c5570b5fcdc40b06f64c3d24814
Shachar <shachar.ratzabi.147@gmail.com>
2023-01-14 12:06:17 +0200

    if there's more than 1 filters - show it summerized

--- src/components/triplan-header/filter-indications/filter-indications.tsx
+++ src/components/triplan-header/filter-indications/filter-indications.tsx
@@ -24,38 +24,62 @@ function FilterIndications(props: FilterIndicationsProps) {
 	if (showOnlyEventsWithNoOpeningHours) totalFilters += 1;
 	if (showOnlyEventsWithTodoComplete) totalFilters += 1;
 
+	function renderSingleFilter() {
+		return (
+			<div className={getClasses('filter-tags-container flex-row gap-8', eventStore.isRtl && 'direction-rtl')}>
+				{showOnlyEventsWithNoLocation && (
+					<TriplanTag
+						text={TranslateService.translate(eventStore, 'SHOW_ONLY_EVENTS_WITH_NO_LOCATION.FILTER_TAG')}
+						onDelete={() => {
+							eventStore.setShowOnlyEventsWithNoLocation(false);
+						}}
+					/>
+				)}
+				{showOnlyEventsWithNoOpeningHours && (
+					<TriplanTag
+						text={TranslateService.translate(
+							eventStore,
+							'SHOW_ONLY_EVENTS_WITH_NO_OPENING_HOURS.FILTER_TAG'
+						)}
+						onDelete={() => {
+							eventStore.setShowOnlyEventsWithNoOpeningHours(false);
+						}}
+					/>
+				)}
+				{showOnlyEventsWithTodoComplete && (
+					<TriplanTag
+						text={TranslateService.translate(eventStore, 'SHOW_ONLY_EVENTS_WITH_TODO_COMPLETE.FILTER_TAG')}
+						onDelete={() => {
+							eventStore.setShowOnlyEventsWithTodoComplete(false);
+						}}
+					/>
+				)}
+			</div>
+		);
+	}
+
 	if (totalFilters === 0) {
 		return null;
 	}
-
-	return (
-		<div className={getClasses('filter-tags-container flex-row gap-8', eventStore.isRtl && 'direction-rtl')}>
-			{showOnlyEventsWithNoLocation && (
+	if (totalFilters === 1) {
+		return renderSingleFilter();
+	} else {
+		return (
+			<div className={getClasses('filter-tags-container flex-row gap-8', eventStore.isRtl && 'direction-rtl')}>
 				<TriplanTag
-					text={TranslateService.translate(eventStore, 'SHOW_ONLY_EVENTS_WITH_NO_LOCATION.FILTER_TAG')}
+					text={TranslateService.translate(eventStore, 'FILTER_INDICATIONS.MULTIPLE_FILTERS').replace(
+						'{{num}}',
+						totalFilters
+					)}
 					onDelete={() => {
+						eventStore.setShowOnlyEventsWithNoOpeningHours(false);
+						eventStore.setShowOnlyEventsWithTodoComplete(false);
 						eventStore.setShowOnlyEventsWithNoLocation(false);
 					}}
 				/>
-			)}
-			{showOnlyEventsWithNoOpeningHours && (
-				<TriplanTag
-					text={TranslateService.translate(eventStore, 'SHOW_ONLY_EVENTS_WITH_NO_OPENING_HOURS.FILTER_TAG')}
-					onDelete={() => {
-						eventStore.setShowOnlyEventsWithNoOpeningHours(false);
-					}}
-				/>
-			)}
-			{showOnlyEventsWithTodoComplete && (
-				<TriplanTag
-					text={TranslateService.translate(eventStore, 'SHOW_ONLY_EVENTS_WITH_TODO_COMPLETE.FILTER_TAG')}
-					onDelete={() => {
-						eventStore.setShowOnlyEventsWithTodoComplete(false);
-					}}
-				/>
-			)}
-		</div>
-	);
+			</div>
+		);
+	}
 }
 
 export default observer(FilterIndications);
