446945ce60de90b76df81bc136b0d28e86c733a1
Shachar Ratzabi Mac <Shachar147@gmail.com>
2023-03-14 21:54:21 +0200

    test pwa

--- src/index.js
+++ src/index.js
@@ -546,6 +546,19 @@ const RootRouter = () => {
 		return window.location.href.indexOf('localhost') !== -1 || window.location.href.indexOf('0.0.0.0') !== -1;
 	}
 
+	if ('serviceWorker' in navigator) {
+		window.addEventListener('load', () => {
+			navigator.serviceWorker
+				.register('/service-worker.js')
+				.then((registration) => {
+					console.log('Service worker registered:', registration);
+				})
+				.catch((error) => {
+					console.error('Service worker registration failed:', error);
+				});
+		});
+	}
+
 	return (
 		<>
 			<BrowserRouter>
