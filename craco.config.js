4a484cce6d120170b9654e08f05c1c29f892907b
Yahav Petel <yahavpetel@gmail.com>
2024-08-08 23:40:23 +0300

    craco config

---
+++ craco.config.js
@@ -0,0 +1,9 @@
+// craco.config.js
+module.exports = {
+    webpack: {
+        configure: (webpackConfig, { env, paths }) => {
+            // Customize the webpack configuration here
+            return webpackConfig;
+        },
+    },
+};
