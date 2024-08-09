503ccd8dec7abfb9acc2c7428f0447ea7eac5197
Yahav Petel <yahavpetel@gmail.com>
2024-08-09 16:43:15 +0300

    Specify npm version in package.json

--- craco.config.js
+++ craco.config.js
@@ -2,10 +2,34 @@ const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin'
 
 module.exports = {
     webpack: {
-        plugins: [
-            ...(process.env.NODE_ENV === 'development' ? [new ReactRefreshWebpackPlugin({
-                overlay: false, // Disable the default overlay to avoid issues
-            })] : []),
-        ],
+        configure: (webpackConfig, { env }) => {
+            if (env === 'production') {
+                // Filter out ReactRefreshWebpackPlugin
+                webpackConfig.plugins = webpackConfig.plugins.filter(
+                    (plugin) => plugin.constructor.name !== 'ReactRefreshWebpackPlugin'
+                );
+
+                // Remove any instances of react-refresh runtime
+                webpackConfig.module.rules.forEach((rule) => {
+                    if (rule.oneOf) {
+                        rule.oneOf.forEach((subRule) => {
+                            if (
+                                subRule.loader &&
+                                subRule.loader.includes('babel-loader') &&
+                                subRule.options &&
+                                subRule.options.plugins
+                            ) {
+                                subRule.options.plugins = subRule.options.plugins.filter(
+                                    (plugin) =>
+                                        typeof plugin !== 'string' ||
+                                        !plugin.includes('react-refresh/babel')
+                                );
+                            }
+                        });
+                    }
+                });
+            }
+            return webpackConfig;
+        },
     },
 };
