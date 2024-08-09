fc37cc329cd7f24b6fe0ec6c1b33a30066bc5333
Yahav Petel <yahavpetel@gmail.com>
2024-08-09 10:37:26 +0300

    wip

--- craco.config.js
+++ craco.config.js
@@ -3,7 +3,9 @@ const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin'
 module.exports = {
     webpack: {
         plugins: [
-            ...(process.env.NODE_ENV === 'development' ? [new ReactRefreshWebpackPlugin()] : []),
+            ...(process.env.NODE_ENV === 'development' ? [new ReactRefreshWebpackPlugin({
+                overlay: false, // Disable the default overlay to avoid issues
+            })] : []),
         ],
     },
 };
