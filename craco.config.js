abd71cbf829ccfab3d00ce1f1092d094cc5850a4
Yahav Petel <yahavpetel@gmail.com>
2024-08-18 20:17:33 +0300

    wip

--- craco.config.js
+++ craco.config.js
@@ -6,5 +6,12 @@ module.exports = {
             '@src': path.resolve(__dirname, 'src'),
             '@node_modules': path.resolve(__dirname, 'node_modules'),
         },
+        configure: (webpackConfig) => {
+            // Disable babel-loader
+            webpackConfig.module.rules = webpackConfig.module.rules.filter(
+                (rule) => !(rule.loader && rule.loader.includes('babel-loader'))
+            );
+            return webpackConfig;
+        },
     },
 };
