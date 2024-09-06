6a7cefe477879084a29b1778200d5e007b7341eb
Yahav Petel <yahavpetel@gmail.com>
2024-09-07 02:14:59 +0300

    wip

--- src/v2/views/login-page/login-page.tsx
+++ src/v2/views/login-page/login-page.tsx
@@ -30,6 +30,7 @@ function LoginPageV2() {
     const navigate = useNavigate();
     const usernameRef = useRef<HTMLInputElement>(null);
     const passwordRef = useRef<HTMLInputElement>(null);
+    const passwordAgainRef = useRef<HTMLInputElement>(null);
     const [validating, setValidating] = useState(false);
     const [errorField, setErrorField] = useState(defaultErrorField);
     const [error, setError] = useState('');
@@ -79,7 +80,6 @@ function LoginPageV2() {
         window.location.href = redirectTo;
     }
 
-
     function renderLoginForm(){
         return (
         <div className="login-form">
@@ -116,20 +116,70 @@ function LoginPageV2() {
                 />
                 <div className="width-300 text-align-center">
                     {TranslateService.translate(eventStore, 'REGISTER_PREFIX')}{' '}
-                    <Link data-testid='register' onClick={() => rootStore.navigateToTabOnLoginPage('register')}>
+                    <a href={"#register"} data-testid='register' onClick={() => rootStore.navigateToTabOnLoginPage('register')}>
                         {TranslateService.translate(eventStore, 'REGISTER')}
-                    </Link>
+                    </a>
                 </div>
             </div>
         </div>
         )
     }
 
+    function renderRegisterForm(){
+        return (
+            <div className="register-form">
+                {message_block}
+                {error_block}
+                {renderField({
+                    name: 'username',
+                    placeholder: TranslateService.translate(eventStore, 'USERNAME'),
+                    icon: 'user',
+                    ref: usernameRef,
+                })}
+                {renderField({
+                    name: 'password',
+                    placeholder: TranslateService.translate(eventStore, 'PASSWORD'),
+                    type: 'password',
+                    icon: 'lock',
+                    ref: passwordRef,
+                })}
+                {renderField({
+                    name: 'passwordAgain',
+                    placeholder: TranslateService.translate(eventStore, 'REPEAT_PASSWORD'),
+                    type: 'password',
+                    icon: 'lock',
+                    ref: passwordAgainRef,
+                })}
+                <div className="register-form-buttons">
+                    <Button
+                        text={TranslateService.translate(eventStore, 'REGISTER_BUTTON')}
+                        onClick={register}
+                        flavor={ButtonFlavor.primary}
+                        disabled={validating}
+                        disabledReason={TranslateService.translate(eventStore, 'PLEASE_WAIT_WHILE_SAVING')}
+                    />
+                    <div className="width-300 text-align-center">
+                        {TranslateService.translate(eventStore, 'LOGIN_PREFIX')}{' '}
+                        <a href={"#login"} data-testid='login' onClick={() => rootStore.navigateToTabOnLoginPage('login')}>
+                            {TranslateService.translate(eventStore, 'LOGIN_LINK')}
+                        </a>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+
     const onKeyDown = (keyCode: number) => {
         if (keyCode === 13) {
-            login();
+            if (activeTab == 'login'){
+                login()
+            } else {
+                register();
+            }
         }
     };
+
+    // region main cations
 
     const login = () => {
         // if we're already trying to perform login, do not try again.
@@ -187,6 +237,77 @@ function LoginPageV2() {
         }
     };
 
+    const register = () => {
+        // if we're already trying to perform login, do not try again.
+        if (validating) return;
+
+        const username = usernameRef.current?.value ?? '';
+        const password = passwordRef.current?.value ?? '';
+        const passwordAgain = passwordAgainRef.current?.value ?? '';
+
+        // validate inputs
+        if (username.length === 0) {
+            setError('USERNAME_EMPTY');
+            setErrorField({ ...errorField, username: true });
+        } else if (password.length === 0 || passwordAgain.length == 0) {
+            setError('PASSWORD_EMPTY');
+            if (password.length === 0 && passwordAgain.length === 0) {
+                setErrorField({ ...errorField, password: true, passwordAgain: true });
+            } else if (password.length === 0) {
+                setErrorField({ ...errorField, password: true });
+            } else {
+                setErrorField({ ...errorField, passwordAgain: true });
+            }
+        } else if (password !== passwordAgain) {
+            setError('PASSWORDS_NOT_MATCH');
+            setErrorField({ ...errorField, password: true, passwordAgain: true });
+        } else {
+            setErrorField(defaultErrorField);
+            setError(""); // new
+            setValidating(true);
+
+            apiPostWithCallback(
+                '/auth/signup',
+                {
+                    username: username,
+                    password: password,
+                },
+                async function (res: any) {
+                    if (res && res.error) {
+                        setError(res.error);
+                    } else {
+                        setMessage('REGISTERED_SUCCESSFULLY');
+                        setTimeout(
+                             () => {
+                                window.location.href = '/login';
+                            },
+                            LOGIN_DELAY,
+                        );
+                    }
+                },
+                (err: any) => {
+                    if (err.response && err.response.data && err.response.data.message) {
+                        const message = err.response.data.message;
+                        setError(typeof message === 'object' ? message.join('<br>') : message);
+
+                        console.log(err, message);
+
+                        if (err.response.data.statusCode && [404].indexOf(err.response.data.statusCode) !== -1) {
+                            setError('Network Error');
+                        }
+                    } else {
+                        setError('Network Error');
+                    }
+                },
+                function () {
+                    setValidating(false);
+                }
+            );
+        }
+    };
+
+    // endregion
+
     function renderField({ name, placeholder, icon, type = 'text', ref }: any) {
         return (
             <div className="field flex-column">
@@ -195,7 +316,7 @@ function LoginPageV2() {
                     name={name}
                     className="textInput"
                     type={type}
-                    placeholder={eventStore.isMobile ? placeholder : TranslateService.translate(eventStore, 'TYPE_YOUR_X', {
+                    placeholder={eventStore.isMobile || (eventStore.isEnglish && placeholder == TranslateService.translate(eventStore, 'REPEAT_PASSWORD')) ? placeholder : TranslateService.translate(eventStore, 'TYPE_YOUR_X', {
                         X: placeholder
                     })}
                     autoComplete="off"
@@ -281,9 +402,24 @@ function LoginPageV2() {
 
     function renderRegisterContainer(){
         return (
-            <div className="register-page-container">
-                register
-            </div>
+            <>
+                <div className="register-page-container">
+                    <img src={images[0]} />
+                    {/*<Carousel showThumbs={false} showIndicators={false} infiniteLoop autoPlay>*/}
+                    {/*    {images.map((image, index) => (*/}
+                    {/*        <img src={image} />*/}
+                    {/*    ))}*/}
+                    {/*</Carousel>*/}
+                    <div className="divider"/>
+                    <div className="flex-column gap-8">
+                        <div className="flex-column block-title">
+                            <h3>{TranslateService.translate(eventStore, 'LOGIN_PAGE_V2.REGISTER.TITLE')}</h3>
+                            <span className="white-space-pre-line max-width-350">{TranslateService.translate(eventStore, 'LOGIN_PAGE_V2.REGISTER.DESCRIPTION')}</span>
+                        </div>
+                        {renderRegisterForm()}
+                    </div>
+                </div>
+            </>
         )
     }
 
