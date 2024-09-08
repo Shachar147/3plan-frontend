553ff3e34f477e9c3d662883e45109475d0ce039
ShacharRatzabi <Shachar147@gmail.com>
2024-09-08 19:52:14 +0300

    wip

---
+++ src/components/triplan-sidebar/sidebar-search/sidebar-search.tsx
@@ -0,0 +1,26 @@
+import React, {useContext} from "react";
+import TriplanSearch from "../../triplan-header/triplan-search/triplan-search";
+import TranslateService from "../../../services/translate-service";
+import {observer} from "mobx-react";
+import {eventStoreContext} from "../../../stores/events-store";
+
+function SidebarSearch() {
+    const eventStore = useContext(eventStoreContext);
+
+    console.log("heree", eventStore.sidebarSearchValue);
+
+    return (
+        <div className="sidebar-search-container">
+            <TriplanSearch
+                value={eventStore.sidebarSearchValue}
+                onChange={(val: any) => eventStore.setSidebarSearchValue(val)}
+                placeholder={TranslateService.translate(eventStore, 'SIDEBAR_SEARCH_PLACEHOLDER')}
+                resetCallback={() => {
+                    eventStore.setSidebarSearchValue("");
+                }}
+            />
+        </div>
+    );
+}
+
+export default observer(SidebarSearch);
