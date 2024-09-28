09d04430916e416995c4b7b2c4bfc3aa282d94b4
ShacharRatzabi <Shachar147@gmail.com>
2024-09-28 23:19:31 +0300

    wip

--- src/v2/admin/components/add-poi-form/add-poi-form.tsx
+++ src/v2/admin/components/add-poi-form/add-poi-form.tsx
@@ -7,14 +7,29 @@ import { eventStoreContext } from "../../../../stores/events-store";
 import { Carousel } from 'react-responsive-carousel';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
 import { Image } from "../../../components/point-of-interest/point-of-interest";
-import {ReactModalRenderHelper} from "../../../../services/react-modal-service";
+import ReactModalService, {ReactModalRenderHelper} from "../../../../services/react-modal-service";
 import {getDefaultCategories} from "../../../../utils/defaults";
+import LocationInput from "../../../../components/inputs/location-input/location-input";
 
 function POIForm() {
     const eventStore = useContext(eventStoreContext);
+
+    const fields = [
+        { name: 'more_info', label: TranslateService.translate(eventStore, 'SOURCE_OR_LINK'), type: 'text', isLink: true },
+        { name: 'name', label: TranslateService.translate(eventStore, 'EVENT_NAME'), type: 'text', isRequired: true },
+        { name: 'location', label: TranslateService.translate(eventStore, 'ADMIN_MANAGE_ITEM.LOCATION'), type: 'location-selector', isRequired: true },
+        { name: 'description', label: TranslateService.translate(eventStore, 'ADMIN_MANAGE_ITEM.DESCRIPTION'), type: 'textarea' },
+        { name: 'category', label: TranslateService.translate(eventStore, 'TEMPLATE.CATEGORY'), type: 'category-selector' },
+        // { name: 'rate.quantity', label: 'Rate Quantity', type: 'number' },
+        // { name: 'rate.rating', label: 'Rate Rating (Out of 5)', type: 'number', max: 5, min: -1 },
+        { name: 'price', label: 'Price', type: 'number' },
+        { name: 'currency', label: 'Currency', type: 'text' },
+        { name: 'images', label: TranslateService.translate(eventStore, 'MODALS.IMAGES'), type: 'image-upload', isRequired: true}
+    ];
+
     const [formData, setFormData] = useState({
-        name: '',
-        location: '',
+        name: undefined, // required
+        location: undefined, // required
         description: '',
         source: 'System',
         more_info: '',
@@ -25,7 +40,7 @@ function POIForm() {
         },
         price: undefined,
         currency: '',
-        images: [] as File[],
+        images: [] as File[], // required
         imagePaths: [] as string[],
     });
     const [renderCounter, setRenderCounter] = useState(0);
@@ -67,10 +82,13 @@ function POIForm() {
     const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
         let { name, value } = e.target;
         if (name == 'price' || name == 'rate'){
-            if (Number(value) < 0){
+            if (value == undefined || Number(value) < 0){
                 value = undefined;
                 e.target.value = undefined;
             }
+        }
+        if (name == 'location' && (!value?.latitude || !value?.longitude)) {
+            value = undefined;
         }
         let updatedFormData;
 
@@ -90,15 +108,47 @@ function POIForm() {
                 };
             }
         } else {
+            if (value == "") {
+                value = undefined;
+            }
             updatedFormData = { ...formData, [name]: value };
         }
-        console.log("hereeee", updatedFormData);
-
+        console.log("hereee", updatedFormData);
         setFormData(updatedFormData);
     };
 
+    function validateBeforeSubmit(){
+        let isOk = true;
+        for (let i = 0; i < fields.length; i++){
+            const field = fields[i];
+
+            const hasValues = field.name == 'location' ? !!formData[field.name] : (formData[field.name]?.length ?? 0) > 0;
+
+            if (field.isRequired && !hasValues){
+                ReactModalService.internal.alertMessage(
+                    eventStore,
+                    'MODALS.ERROR.TITLE',
+                    'PLEASE_FILL_IN_X_BEFORE_YOU_PROCEED',
+                    'error',
+                    {
+                        X: field.label
+                    }
+                );
+                isOk = false;
+                break;
+            }
+        }
+
+        return isOk;
+    }
+
     const handleSubmit = async (e: React.FormEvent) => {
         e.preventDefault();
+
+        if (!validateBeforeSubmit()){
+            return false;
+        }
+
         try {
             await handleImageProcessing(); // Process images first
 
@@ -137,69 +187,120 @@ function POIForm() {
         );
     }
 
-    const fields = [
-        { name: 'more_info', label: TranslateService.translate(eventStore, 'SOURCE_OR_LINK'), type: 'text', isLink: true },
-        { name: 'name', label: TranslateService.translate(eventStore, 'ADMIN_MANAGE_ITEM.NAME'), type: 'text' },
-        { name: 'location', label: TranslateService.translate(eventStore, 'ADMIN_MANAGE_ITEM.LOCATION'), type: 'text' },
-        { name: 'description', label: TranslateService.translate(eventStore, 'ADMIN_MANAGE_ITEM.DESCRIPTION'), type: 'textarea' },
-        { name: 'category', label: TranslateService.translate(eventStore, 'TEMPLATE.CATEGORY'), type: 'category-selector' },
-        // { name: 'rate.quantity', label: 'Rate Quantity', type: 'number' },
-        // { name: 'rate.rating', label: 'Rate Rating (Out of 5)', type: 'number', max: 5, min: -1 },
-        { name: 'price', label: 'Price', type: 'number' },
-        { name: 'currency', label: 'Currency', type: 'text' },
-    ];
+    function renderInput({ type, name, max, isLink}: { type: string, name: string, max?: number, isLink?: boolean }){
+        if (type == 'textarea'){
+            return (
+                <textarea name={name} value={formData[name]} onChange={handleChange} required />
+            )
+        }
 
-    return (
-        <div className="add-poi-form-container">
-            <div className="add-poi-form">
-                {fields.map(({ name, label, type, max, isLink }) => (
-                    <div key={name}>
-                        <label>{label}:</label>
-                        {type === 'textarea' ? (
-                            <textarea name={name} value={formData[name]} onChange={handleChange} required />
-                        ) :
-                        type === 'category-selector' ? (
-                            ReactModalRenderHelper.renderSelector(
-                                eventStore,
-                                'category',
-                                {
-                                    name,
-                                    value: formData[name],
-                                    // @ts-ignore
-                                    onChange: (data) => handleChange({
-                                        target: {
-                                            name,
-                                            value: data ? getDefaultCategories(eventStore).find((c) => c.id == data.value)?.titleKey : 'CATEGORY.GENERAL'
-                                        }
-                                    })
-                                },
-                                // { placeholderKey: 'SELECT_CATEGORY_PLACEHOLDER' },
-                                // undefined,
-                                getDefaultCategories(eventStore).sort((a, b) => a.id - b.id)
-                                .map((x, index) => ({
-                                    value: x.id.toString(),
-                                    label: x.icon ? `${x.icon} ${x.title}` : x.title,
-                                }))
-                            )
-                        ) :
-                        (
-                            <input type={type} name={name} value={formData[name]} onChange={(e) => {
-                                if (isLink && e.target.value.length > 0 && !(e.target.value.startsWith("http://") || e.target.value.startsWith("https://"))){
-                                    return false;
-                                }
-                                handleChange(e);
-                                return true;
-                            }} max={max} required={type !== 'text'} />
-                        )}
-                    </div>
-                ))}
+        if (type == 'image-upload') {
+            return (
                 <div className="flex-column gap-4">
-                    <label>{TranslateService.translate(eventStore, 'MODALS.IMAGES')}:</label>
                     {formData.imagePaths.length > 0 && renderPreview()}
                     <label className="file-label" htmlFor="file-upload">{TranslateService.translate(eventStore, 'CHOOSE_A_FILE')}</label>
                     <input type="file" id="file-upload" className="file-input" multiple onChange={handleImageUpload} />
                     <span id="file-name">{TranslateService.translate(eventStore, 'NO_FILE_CHOSEN')}</span>
                 </div>
+            )
+        }
+
+        if (type == 'location-selector') {
+            const initLocation = () => {
+                // @ts-ignore
+                window.initLocationPicker('location-input', 'selectedLocation', () => {
+                    handleChange({
+                        target:{
+                            name,
+                            value: window['selectedLocation']
+                        }
+                    })
+                }, eventStore);
+            };
+
+            const setManualLocation = () => {
+                // @ts-ignore
+                window.setManualLocation('location-input', 'selectedLocation', eventStore);
+                handleChange({
+                    target:{
+                        name,
+                        value: window['selectedLocation']
+                    }
+                })
+            };
+
+            return (
+                <LocationInput
+                    // id={extra.id}
+                    className="location-input"
+                    ref={eventStore.modalValuesRefs['location']}
+                    modalValueName="location"
+                    onClick={initLocation}
+                    onKeyUp={setManualLocation}
+                    placeholder={TranslateService.translate(eventStore, 'MODALS.LOCATION.PLACEHOLDER')}
+                    // placeholderKey={extra.placeholderKey}
+                    autoComplete="off"
+                    readOnly={false}
+                    disabled={false}
+                    // showOnMapLink={extra.readOnly}
+                    // eventId={extra.eventId}
+                />
+            )
+        }
+
+        if (type === 'category-selector'){
+            return (
+                ReactModalRenderHelper.renderSelector(
+                    eventStore,
+                    'category',
+                    {
+                        name,
+                        value: formData[name],
+                        // @ts-ignore
+                        onChange: (data) => handleChange({
+                            target: {
+                                name,
+                                value: data ? getDefaultCategories(eventStore).find((c) => c.id == data.value)?.titleKey : 'CATEGORY.GENERAL'
+                            }
+                        })
+                    },
+                    // { placeholderKey: 'SELECT_CATEGORY_PLACEHOLDER' },
+                    // undefined,
+                    getDefaultCategories(eventStore).sort((a, b) => a.id - b.id)
+                        .map((x, index) => ({
+                            value: x.id.toString(),
+                            label: x.icon ? `${x.icon} ${x.title}` : x.title,
+                        }))
+                )
+            );
+        }
+
+        return (
+            <input type={type} name={name} value={formData[name]} onChange={(e) => {
+                if (isLink && e.target.value.length > 0 && !(e.target.value.startsWith("http://") || e.target.value.startsWith("https://"))){
+                    return false;
+                }
+                handleChange(e);
+                return true;
+            }} max={max} required={type !== 'text'} />
+        )
+    }
+
+    function renderLabel(label: string, isRequired?: boolean) {
+        return (
+            <label>{label}:{isRequired && <span className="red-color margin-inline-start-3">*</span>}</label>
+        )
+    }
+
+    return (
+        <div className="add-poi-form-container">
+            <div className="add-poi-form">
+                {fields.map(({ name, label, type, max, isLink, isRequired }) => (
+                    <div key={name}>
+                        {renderLabel(label, isRequired)}
+                        {renderInput({ name, type, max, isLink })}
+                    </div>
+                ))}
                 <Button flavor={ButtonFlavor.primary} onClick={handleSubmit} text={TranslateService.translate(eventStore, 'MODALS.SAVE')} />
             </div>
         </div>
