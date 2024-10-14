cbbfdc42c804d4b6ac376e606b1f8deba6cac6b9
ShacharRatzabi <Shachar147@gmail.com>
2024-10-14 23:12:50 +0300

    wip

--- src/admin/stores/admin-store.ts
+++ src/admin/stores/admin-store.ts
@@ -25,20 +25,18 @@ export class AdminStore {
 
 	// --- init -------------------------------------------------
 	init() {
-		// moved to comment for now due to a problem - trying to query admin-statistics while user is not even logged in.
-
-		// const promises = [TriplanTinderApiService.getPlacesByDestination(), DataServices.DBService.getUserStats()];
-		// Promise.all(promises).then((results) => {
-		// 	// @ts-ignore
-		// 	const places: GetPlacesByDestinationResult = results[0];
-		// 	// @ts-ignore
-		// 	const stats: any[] = results?.[1] ?? [];
-		// 	runInAction(() => {
-		// 		this.placesByDestination = observable.map<string, TinderItem[]>(places?.data);
-		// 		this.userStats = observable.array<any[]>(stats);
-		// 		this.hasInit = true;
-		// 	});
-		// });
+		const promises = [TriplanTinderApiService.getPlacesByDestination(), DataServices.DBService.getUserStats()];
+		Promise.all(promises).then((results) => {
+			// @ts-ignore
+			const places: GetPlacesByDestinationResult = results[0];
+			// @ts-ignore
+			const stats: any[] = results?.[1] ?? [];
+			runInAction(() => {
+				this.placesByDestination = observable.map<string, TinderItem[]>(places?.data);
+				this.userStats = observable.array<any[]>(stats);
+				this.hasInit = true;
+			});
+		});
 	}
 
 	// --- computed ---------------------------------------------
